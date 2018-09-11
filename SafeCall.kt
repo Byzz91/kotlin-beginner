665cfa506c6d51767e901d640b55133b2c3c1fae
byzz <byzz@inven.co.kr>
2018-09-11 13:55:44 +0900

    SafeCall

--- SafeCall.kt
+++ SafeCall.kt
@@ -8,3 +8,15 @@ fun safeCallExample() {
 
 // SafeCall
 fun getSize(a: String?) = a?.length
+
+
+// SafeCall의 장점, null 인지 if 체크가 필요한 부분에서 간단한 코딩이 가능해진다.
+data class AAA(val bbb: BBB?)
+data class BBB(val ccc: CCC?)
+data class CCC(val name: String?)
+
+@Test
+fun test() {
+    val aaa: AAA = AAA(BBB(null))
+    val name = aaa?.bbb?.ccc
+}
