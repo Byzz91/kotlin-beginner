8e16553ff664539b7165a2955bf5cd19a474ff3e
byzz <byzz@inven.co.kr>
2018-09-12 16:04:08 +0900

    init 블록의 단축

--- src/PrimayConstructorValidate.kt
+++ src/PrimayConstructorValidate.kt
@@ -13,3 +13,14 @@ class UserInfo(name: String = "Name", age: Int = 0, birthday: String = "2001-00-
         this.birthday = birthday.takeIf { it.isNotEmpty() } ?: ""
     }
 }
+
+/**
+ * init block을 활용한 validate 체크
+ * init 블록의 사용을 줄일 수 있다.
+ */
+class UserInfo2(name: String = "Name", age: Int = 0, birthday: String = "2001-00-00") {
+    // 전역 변수를 정의
+    val name: String = name.takeIf { it.isNotEmpty() } ?: ""
+    val age: Int = age.takeIf { it > 0 } ?: 0
+    val birthday: String = birthday.takeIf { it.isNotEmpty() } ?: ""
+}
