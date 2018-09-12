cc19ae7721a273aa0e693a81882e575a324906c8
byzz <byzz@inven.co.kr>
2018-09-12 11:35:58 +0900

    kotlin 라이브러리를 이용한 null 예외처리

---
+++ src/RemoveNullFromList.kt
@@ -0,0 +1,19 @@
+val list: List<String?> = mutableListOf("A", null, "C")
+list.filter { it != null }.forEach { println(it) }
+
+val cloneListWithoutNull: List<String> = list.filterNotNull()
+cloneListWithoutNull.forEach {
+    println(it)
+}
+
+data class User(val name: String, val age: Int, val id: String)
+
+fun nullException() {
+    val user: User? = User("eyedroot", 28, "eyedroot")
+
+    user?.let {
+        data.name = it.name
+        data.age = it.age
+        data.id = it.id
+    }
+}
